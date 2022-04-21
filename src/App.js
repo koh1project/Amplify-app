df1d102ea228349f67b45e11481c177d6fe15d83
Koichi <koichi89592@gmail.com>
2022-04-20 17:13:47 -0700

    Change import style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import { API, graphqlOperation, Amplify } from 'aws-amplify'
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 
@@ -35,43 +35,38 @@ const App = () => {
       const todo = { ...formState }
       setTodos([...todos, todo])
       setFormState(initialState)
-      await API.graphql(graphqlOperation(createNoteMutation, {input: todo}))
+      await API.graphql(graphqlOperation(createNoteMutation, { input: todo }))
     } catch (err) {
       console.log('error creating todo:', err)
     }
   }
 
-  return (
-    <div style={styles.container}>
-      <h2>Amplify Todos</h2>
-      <input
-        onChange={event => setInput('name', event.target.value)}
-        style={styles.input}
-        value={formState.name}
-        placeholder="Name"
-      />
-      <input
-        onChange={event => setInput('description', event.target.value)}
-        style={styles.input}
-        value={formState.description}
-        placeholder="Description"
-      />
-      <button style={styles.button} onClick={addTodo}>Create Todo</button>
-      {
-        todos.map((todo, index) => (
-          <div key={todo.id ? todo.id : index} style={styles.todo}>
-            <p style={styles.todoName}>{todo.name}</p>
-            <p style={styles.todoDescription}>{todo.description}</p>
-          </div>
-        ))
-      }
-    </div>
+  return ( <
+    div style = { styles.container } >
+    <
+    h2 > Amplify Todos < /h2> <
+    input onChange = { event => setInput('name', event.target.value) } style = { styles.input } value = { formState.name } placeholder = "Name" /
+    >
+    <
+    input onChange = { event => setInput('description', event.target.value) } style = { styles.input } value = { formState.description } placeholder = "Description" /
+    >
+    <
+    button style = { styles.button } onClick = { addTodo } > Create Todo < /button> {
+      todos.map((todo, index) => ( <
+        div key = { todo.id ? todo.id : index } style = { styles.todo } >
+        <
+        p style = { styles.todoName } > { todo.name } < /p> <
+        p style = { styles.todoDescription } > { todo.description } < /p> <
+        /div>
+      ))
+    } <
+    /div>
   )
 }
 
 const styles = {
   container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  todo: {  marginBottom: 15 },
+  todo: { marginBottom: 15 },
   input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
   todoName: { fontSize: 20, fontWeight: 'bold' },
   todoDescription: { marginBottom: 0 },
